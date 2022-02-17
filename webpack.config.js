f8fb8c0e22bca399b46a7c866b2e5680c6fc9f83
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-02-17 15:11:36 -0500

    tanspile change

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config')
-
+const createExpoWebpackConfigAsync = require('@expo/webpack-config/addons')
 module.exports = async function (env, argv) {
     const config = await createExpoWebpackConfigAsync(
         {
             ...env,
-            babel: { dangerouslyAddModulePathsToTranspile: ['moti', '@motify'] },
+            babel: { dangerouslyAddModulePathsToTranspile: ['app', 'moti', '@motify'] },
         },
         argv
     )
-
     return config
-}
+} 
