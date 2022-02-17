dfecea74e4fe1f8e052c9502dba6e5509b44e0f5
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-02-17 15:14:48 -0500

    whitespace

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = async function (env, argv) {
     const config = await createExpoWebpackConfigAsync(
         {
             ...env,
-            babel: { dangerouslyAddModulePathsToTranspile: ['app', 'moti', '@motify'] },
+            babel: { dangerouslyAddModulePathsToTranspile: ['moti', '@motify'] },
         },
         argv
     )
