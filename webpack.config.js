fbc951179e1d797b40c491b7cb0db720f68a7938
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-02-18 09:56:57 -0500

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = async function (env, argv) {
     const config = await createExpoWebpackConfigAsync(
         {
             ...env,
-            babel: { dangerouslyAddModulePathsToTranspile: ["moti"] },
+            babel: { dangerouslyAddModulePathsToTranspile: ["moti| @motify/core|@motify/components"] },
         },
         argv
     );
